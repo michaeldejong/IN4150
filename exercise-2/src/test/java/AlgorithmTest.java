0ef3ff44f319ecfcca94457d103ffa60225ed7c6
Michael de Jong <michaelj@minicom.nl>
2013-03-14 17:10:50 +0100

    Fixed test

--- exercise-2/src/test/java/AlgorithmTest.java
+++ exercise-2/src/test/java/AlgorithmTest.java
@@ -9,27 +9,21 @@ import org.junit.Test;
 
 public class AlgorithmTest {
 
-	private NodeController c1, c2, c3;
-	private ChandyLamportGlobalStateAlgorithm a1, a2, a3;
+	private NodeController c1, c2;
+	private ChandyLamportGlobalStateAlgorithm a1, a2;
 
 	@Before
 	public void setUp() throws IOException {
 		a1 = new ChandyLamportGlobalStateAlgorithm();
 		a2 = new ChandyLamportGlobalStateAlgorithm();
-		a3 = new ChandyLamportGlobalStateAlgorithm();
 
 		InetAddress localHost = InetAddress.getLocalHost();
 		c1 = new NodeController(localHost, true, a1);
 		c2 = new NodeController(localHost, true, a2);
-		c3 = new NodeController(localHost, true, a3);
 
 		// fully connected network
 		c1.addRemote(c2.getLocalAddress());
-		c1.addRemote(c3.getLocalAddress());
 		c2.addRemote(c1.getLocalAddress());
-		c2.addRemote(c3.getLocalAddress());
-		c3.addRemote(c1.getLocalAddress());
-		c3.addRemote(c2.getLocalAddress());
 	}
 
 	@Test
@@ -37,8 +31,6 @@ public class AlgorithmTest {
 		a1.start();
 		Thread.sleep(5);
 		a2.start();
-		Thread.sleep(5);
-		a3.start();
 
 		Thread.sleep(200);
 		
