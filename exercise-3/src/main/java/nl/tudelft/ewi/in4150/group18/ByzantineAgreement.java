ef25728257a82896be9ffbef516f5f81bf2bb89c
Michael de Jong <michaelj@minicom.nl>
2013-04-29 16:21:54 +0200

    Added assignment 3 to the repository.

---
+++ exercise-3/src/main/java/nl/tudelft/ewi/in4150/group18/ByzantineAgreement.java
@@ -0,0 +1,21 @@
+package nl.tudelft.ewi.in4150.group18;
+
+import nl.tudelft.in4150.group18.DistributedAlgorithm;
+import nl.tudelft.in4150.group18.common.IRemoteObject.IMessage;
+import nl.tudelft.in4150.group18.network.Address;
+
+public class ByzantineAgreement extends DistributedAlgorithm {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onMessage(IMessage message, Address from) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
